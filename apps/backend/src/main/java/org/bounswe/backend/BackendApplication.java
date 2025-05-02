d7ed3eb7c2da62a0bd0f207171b8dbf83ccc0011
CotSezer <cotsezer2000@gmail.com>
2025-05-03 01:10:00 +0300

    openapi definition is added into main class.

--- apps/backend/src/main/java/org/bounswe/backend/BackendApplication.java
+++ apps/backend/src/main/java/org/bounswe/backend/BackendApplication.java
@@ -1,9 +1,11 @@
 package org.bounswe.backend;
 
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Ethical Job Board API", version = "1.0"))
 @SpringBootApplication
 public class BackendApplication {
 
