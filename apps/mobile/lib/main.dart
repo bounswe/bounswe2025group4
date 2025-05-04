ed4c4e71ca0b048bb6449d5f87f8ac8bf0aacd22
Ömer Faruk Koramaz <omer.koramaz@Omer-MacBook-Air-8.local>
2025-05-04 15:07:13 +0300

    fix: remove .env dependency

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'core/providers/auth_provider.dart';
 import 'features/profile/screens/profile_page.dart';
 
-void main() async {
-  await dotenv.load(fileName: ".env");
+void main() {
   runApp(const MyApp());
 }
 
