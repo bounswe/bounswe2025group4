e7e1e96ae32e683311ffcfc7dbfbfb5d9b5b607b
Battal HAZAR <115145926+battal255@users.noreply.github.com>
2025-05-04 17:11:09 +0300

    Revert "feat: implement forum entities and DTOs"

--- apps/backend/src/main/java/org/bounswe/backend/tag/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package org.bounswe.backend.tag.repository;
-
-import org.bounswe.backend.tag.entity.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
--- apps/backend/src/main/java/org/bounswe/backend/thread/repository/ThreadRepository.java
+++
@@ -1,7 +0,0 @@
-package org.bounswe.backend.thread.repository;
-
-import org.bounswe.backend.thread.entity.Thread;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ThreadRepository extends JpaRepository<Thread, Long> {
-}
