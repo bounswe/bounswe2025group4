8571ab48e886390dbe73c073e47a4583423ac54a
Asaf31214 <asaf.kanlipicak@outlook.com>
2025-05-04 03:18:50 +0300

    job application endpoints

---
+++ apps/backend/src/main/java/org/bounswe/backend/application/dto/JobApplicationDto.java
@@ -0,0 +1,28 @@
+package org.bounswe.backend.application.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.bounswe.backend.application.enums.JobApplicationStatus;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JobApplicationDto {
+
+    private Long id;
+    
+    @NotNull(message = "Job seeker ID is required")
+    private Long jobSeekerId;
+    
+    @NotNull(message = "Job posting ID is required")
+    private Long jobPostingId;
+    
+    private JobApplicationStatus status;
+    
+    private String feedback;
+    
+    private Date submissionDate;
+}
---
+++ apps/backend/src/main/java/org/bounswe/backend/application/enums/JobApplicationStatus.java
@@ -0,0 +1,7 @@
+package org.bounswe.backend.application.enums;
+
+public enum JobApplicationStatus {
+    PENDING,
+    APPROVED,
+    REJECTED,
+}
