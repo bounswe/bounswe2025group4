716c4e6b312ba4b1b9013068af967b65c2d284b6
Asaf31214 <asaf.kanlipicak@outlook.com>
2025-05-12 02:54:38 +0300

    minor bugfix on dto class placement

---
+++ apps/backend/src/main/java/org/bounswe/backend/user/dto/UpdateMentorshipDto.java
@@ -0,0 +1,7 @@
+package org.bounswe.backend.user.dto;
+
+import org.bounswe.backend.common.enums.MentorshipStatus;
+
+public class UpdateMentorshipDto {
+    public MentorshipStatus mentorshipStatus;
+}
