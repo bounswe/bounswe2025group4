1f11f4eef54d9e5b4e04080d19d61b64403e9fad
ykaydogdu <y.kaan.aydogdu@gmail.com>
2025-05-11 23:55:10 +0300

    feat: enable caching and scheduling in backend application

--- apps/backend/src/main/java/org/bounswe/backend/BackendApplication.java
+++ apps/backend/src/main/java/org/bounswe/backend/BackendApplication.java
@@ -4,7 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
+@EnableScheduling
 @OpenAPIDefinition(info = @Info(title = "Ethical Job Board API", version = "1.0"))
 @SpringBootApplication
 public class BackendApplication {
