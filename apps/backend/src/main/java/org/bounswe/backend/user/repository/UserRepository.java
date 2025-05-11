c18d6c4e04e51890368d1d4d7e8a1d4203b3287d
CotSezer <cotsezer2000@gmail.com>
2025-05-12 00:36:08 +0300

    findByEmail is added.

--- apps/backend/src/main/java/org/bounswe/backend/user/repository/UserRepository.java
+++ apps/backend/src/main/java/org/bounswe/backend/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package org.bounswe.backend.user.repository;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.bounswe.backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
+    Optional<User> findByEmail(@NotBlank @Email String email);
 }
