5d1b54cd1b55f24227fa924d85e36db52476c1c3
Enver Eren <erenenver@outlook.com>
2025-05-12 17:14:33 +0300

    DiscussionThread timestamp, GET thread by id, POST tag seperately

--- apps/backend/src/main/java/org/bounswe/backend/thread/dto/ThreadDto.java
+++ apps/backend/src/main/java/org/bounswe/backend/thread/dto/ThreadDto.java
@@ -1,6 +1,7 @@
 package org.bounswe.backend.thread.dto;
 import lombok.*;
 import java.util.List;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -11,6 +12,10 @@ public class ThreadDto {
     private String title;
     private String body;
     private Long creatorId;
+    private String creatorUsername;
     private List<String> tags;
     private boolean reported;
+    private LocalDateTime createdAt;
+    private LocalDateTime editedAt;
+    private int commentCount;
 }
