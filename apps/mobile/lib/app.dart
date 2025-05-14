9b9bfd84c72e63dac33ef4923dcc027223016923
Gökberk <110094815+gokberkyavuz@users.noreply.github.com>
2025-05-14 23:59:23 +0300

    Changed App logo and name

--- apps/mobile/lib/app.dart
+++ apps/mobile/lib/app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ethical Job Platform',
+      title: 'EthicaJobs',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
