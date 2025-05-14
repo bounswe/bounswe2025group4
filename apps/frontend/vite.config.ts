fd8f9d93ed29690ede7dc63e4410a5ba7b144750
Asaf31214 <asaf.kanlipicak@outlook.com>
2025-05-14 20:33:35 +0300

    vite api base url

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -7,7 +7,7 @@ const viteConfig =  defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://bounswe2025group4-327739565032.europe-west1.run.app',
         changeOrigin: true,
       },
     },
