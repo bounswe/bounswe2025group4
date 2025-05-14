c9c801e2c77e4f03c68139711a5e8ca2d0d96ae8
Gökberk <110094815+gokberkyavuz@users.noreply.github.com>
2025-05-14 23:23:12 +0300

    Added logo to Welcome Screen

--- apps/mobile/lib/core/constants/app_constants.dart
+++ apps/mobile/lib/core/constants/app_constants.dart
@@ -6,15 +6,6 @@ class AppConstants {
   static String get baseUrl => _devBaseUrl;
 
   static String _getBaseUrl() {
-    if (Platform.isAndroid) {
-      // Use 10.0.2.2 for Android Emulator to connect to host localhost
-      return 'https://bounswe2025group4-327739565032.europe-west1.run.app/api';
-    } else if (Platform.isIOS) {
-      // iOS Simulator usually works with localhost
-      return 'https://bounswe2025group4-327739565032.europe-west1.run.app/api';
-    } else {
-      // Default for other platforms (web, desktop - might need adjustment)
-      return 'https://bounswe2025group4-327739565032.europe-west1.run.app/api';
-    }
+    return 'https://bounswe2025group4-327739565032.europe-west1.run.app/api';
   }
 }
