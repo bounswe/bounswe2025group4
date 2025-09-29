7bfdddc97839d620106d4a8551dab7dcdd3ea19d
asafk <asaf.kanlipicak@outlook.com>
2025-09-30 01:07:00 +0300

    spring boot backend initialization

---
+++ apps/jobboard-backend/jobboard-backend/src/test/java/org/bounswe/jobboardbackend/JobboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bounswe.jobboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobboardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
