5804766d2c676f7d7744d0e945e04e10127090c4
CotSezer <cotsezer2000@gmail.com>
2025-10-10 00:18:46 +0300

    feat(model): introduce User, Role and ERole enums

--- apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/jwt/model/ERole.java
+++ apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/jwt/model/ERole.java
@@ -1,4 +1,7 @@
 package org.bounswe.jobboardbackend.jwt.model;
 
 public enum ERole {
+    ROLE_EMPLOYER,
+    ROLE_JOBSEEKER,
+    ROLE_ADMIN
 }
--- apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/jwt/model/Role.java
+++ apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/jwt/model/Role.java
@@ -1,4 +1,21 @@
 package org.bounswe.jobboardbackend.jwt.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity(name="roles")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
 }
