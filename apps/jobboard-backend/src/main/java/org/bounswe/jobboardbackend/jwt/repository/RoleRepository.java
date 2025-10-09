ec1110e86fe5efd9d0953bd6bbaa74841e90b2d4
CotSezer <cotsezer2000@gmail.com>
2025-10-10 00:20:11 +0300

    feat(repo): add UserRepository, RoleRepository, and PasswordResetTokenRepository

--- apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/jwt/repository/RoleRepository.java
+++ apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/jwt/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package org.bounswe.jobboardbackend.jwt.repository;
 
-public interface RoleRepository {
+import org.bounswe.jobboardbackend.jwt.model.ERole;
+import org.bounswe.jobboardbackend.jwt.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
 }
