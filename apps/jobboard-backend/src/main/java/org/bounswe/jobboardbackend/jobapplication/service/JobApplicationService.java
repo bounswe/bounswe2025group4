8402fcaeb6e7c7b2dd6bf94d7d9219339086d929
Enver Eren <erenenver@outlook.com>
2025-10-19 22:42:57 +0300

    Backend sync

--- apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/jobapplication/service/JobApplicationService.java
+++ apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/jobapplication/service/JobApplicationService.java
@@ -20,7 +20,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.google.cloud.storage.*;
 
 import java.io.IOException;
