1981829ed41deb15e6f8a1bacc70b1ab4c213781
Enver Eren <erenenver@outlook.com>
2025-11-01 19:33:18 +0300

    Enums for workplace feature created.

---
+++ apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/workplace/model/enums/EmployerRequestStatus.java
@@ -0,0 +1,7 @@
+package org.bounswe.jobboardbackend.workplace.model.enums;
+
+public enum EmployerRequestStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
---
+++ apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/workplace/model/enums/ReviewReportReason.java
@@ -0,0 +1,8 @@
+package org.bounswe.jobboardbackend.workplace.model.enums;
+
+public enum ReviewReportReason {
+    SPAM,
+    OFFENSIVE,
+    FAKE_REVIEW,
+    OTHER
+}
