890791709599a7f9a6aae909b5d40f645b30c200
CotSezer <cotsezer2000@gmail.com>
2025-12-09 23:58:05 +0300

    refactor(profile): added firstName, lastName, gender and bio fields into RegisterRequest

--- apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/auth/dto/RegisterRequest.java
+++ apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/auth/dto/RegisterRequest.java
@@ -2,6 +2,7 @@ package org.bounswe.jobboardbackend.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,4 +25,18 @@ public class RegisterRequest {
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;
+
+    @NotBlank
+    @Size(min = 2, max = 30)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 2, max = 30)
+    private String lastName;
+
+    @Pattern(regexp = "^(?i)(MALE|FEMALE|OTHER)$")
+    private String gender;
+
+    @Size(max = 250)
+    private String bio;
 }
