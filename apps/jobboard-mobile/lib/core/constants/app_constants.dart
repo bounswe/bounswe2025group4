35829c12e955a1e8f466ebd2b740c423edcc466d
burcukilic <burcuklburcu@hotmail.com>
2025-12-15 01:34:31 +0300

    convert links to prod

--- apps/jobboard-mobile/lib/core/constants/app_constants.dart
+++ apps/jobboard-mobile/lib/core/constants/app_constants.dart
@@ -1,10 +1,10 @@
 class AppConstants {
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://jobboard-backend-dev-728855696411.europe-west1.run.app/api',
+    defaultValue: 'https://jobboard-backend-728855696411.europe-west1.run.app/api',
   );
   static const String baseWsUrl = String.fromEnvironment(
     'API_BASE_WS_URL',
-    defaultValue: 'wss://jobboard-backend-dev-728855696411.europe-west1.run.app/ws-chat/websocket',
+    defaultValue: 'wss://jobboard-backend-728855696411.europe-west1.run.app/ws-chat/websocket',
   );
 }
