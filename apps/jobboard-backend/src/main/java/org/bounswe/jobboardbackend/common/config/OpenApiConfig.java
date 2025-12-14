9cbc02df2a5e04c426a81c6f5c62482a273e161e
Enver Eren <erenenver@outlook.com>
2025-12-14 23:23:02 +0300

    docs(config): configure OpenAPI and Swagger UI setup

---
+++ apps/jobboard-backend/src/main/java/org/bounswe/jobboardbackend/common/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package org.bounswe.jobboardbackend.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info()
+                        .title("JobBoard API")
+                        .description("API documentation for the JobBoard application.")
+                        .version("1.0.0")
+                        .contact(new Contact().name("BounSwe Group 4")
+                                .url("https://github.com/bounswe/bounswe2025group4")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
